38146a79c2f0607fb1561cb6338ff2e216fe1c1a
Moh-Shafi <78737739+Moh-Shafi@users.noreply.github.com>
2023-06-15 19:24:22 +0330

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Mustafa Ceceli-Tut Elimden",
+    artist: "Mustafa Ceceli",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Haram Olasın",
+    artist: "Kürşat Gürel Feat Esra Akaydın",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Canm",
+    artist: "Mustafa Ceceli Yaar pek",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Leylayim Ben Sana",
+    artist: "Naz Dej",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Beni Cok Sev",
+    artist: "Tarkan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Kara Tren_youtube_original",
+    artist: "Yavuz Bingöl",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
